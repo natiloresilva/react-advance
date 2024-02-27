08bff2e6f2f2464cd497fba91af825ee5d7a2bef
natalia <natalialorenasilva@hotmail.com>
2024-02-27 06:38:00 +0100

    pure and impure functions example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { CustomButton } from "./CustomButton";
 
+//Función impura: Siempre cambia su valor de retorno
+const MyDate = () => new Date().toLocaleString();
+
+//Función pura: Nunca cambia su valor de retorno
+const MyComponente = ({ miProp }) => <p>Hello {miProp} </p>;
+
 const App = () => {
   return (
     <>
-      <p>Hola mi Gali </p>
-      <CustomButton onClick={() => console.log("Enviado")}>Enviar</CustomButton>
+      <p>The current time and date is: {MyDate()}</p>
+      <MyComponente miProp={"Galileo"} />
+      <CustomButton onClick={() => console.log("Enviado")}>Send</CustomButton>
     </>
   );
 };
